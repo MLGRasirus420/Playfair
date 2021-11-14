435c33bb86fe2f336f8ac85d67e340e12d84c9db
Martin <darebnik.martin01@gmail.com>
2021-11-14 05:46:34 +0100

    hotovo

--- domaci_ukol_04/rsa.py
+++ domaci_ukol_04/rsa.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PyQt5 import QtGui, uic
 from sympy import randprime, totient
 import timeit
@@ -32,6 +32,13 @@ class MyApp(QMainWindow, Ui_MainWindow):
         self.dLine.setText(str(d))
         self.eLine.setText(str(e))
 
+
+    def check_for_empty_input(self, my_text, msg):
+        if my_text == [''] or my_text == '':
+            self.error_message(msg)
+            return -1
+        
+        
     def encrypt(self, my_text, n, e):
         my_text = self.split_into_list(my_text)
         i = 0
@@ -65,23 +72,80 @@ class MyApp(QMainWindow, Ui_MainWindow):
             
         
     def encodeButton_clicked(self):
-        my_text = self.inputText.toPlainText()
-        n = self.nLine.text()
-        e = self.eLine.text()
-        my_text = self.encrypt(my_text, n, e)
-        finished_text = ''
+        try:
+            my_text = self.inputText.toPlainText()
+            if self.check_for_empty_input(my_text, 'Prázdný vstup!') == -1: return -1
+            n = self.nLine.text()
+            if self.check_for_empty_input(n, 'Prázdný klíč N! Pro šifrování je '
+                                       'potřeba zadat N a E.') == -1: return -1
+            e = self.eLine.text()
+            if self.check_for_empty_input(e, 'Prázdný klíč E! Pro šifrování je '
+                                       'potřeba zadat N a E.') == -1: return -1
+            my_text = self.encrypt(my_text, n, e)
+            finished_text = ''
+            for item in my_text:
+                finished_text += str(item) + ' '
+            finished_text = finished_text.rstrip()
+            self.outputText.setPlainText(finished_text)
+        except ValueError:
+            self.error_message('Něco se pokazilo!')
+        
+        
+    def decrypt(self, my_text, n, d):
+        i = 0
         for item in my_text:
-            finished_text += str(item) + ' '
-        finished_text = finished_text.rstrip()
-        self.outputText.setPlainText(finished_text)
+            my_text[i] = pow(int(item), int(d), int(n))
+            i += 1
+        return my_text
+    
+    
+    def decode_transformation(self, my_text):
+        decoded_text = ''
+        i = 0
+        for item in my_text:
+            my_text[i] = bin(int(item))[2:].zfill(50)
+            i += 1
+        temp = []
+        for item in my_text:
+            temp = [item[i:i+10] for i in range(0, len(item), 10)]
+            i = 0
+            for item in temp:
+                temp[i] = chr(int(item, 2))
+                i += 1
+            for item in temp:
+                decoded_text += str(item)
+        return decoded_text
+    
+    
+    def decodeButton_clicked(self):
+        try:
+            my_text = self.inputText.toPlainText().split(' ')
+            if self.check_for_empty_input(my_text, 'Prázdný vstup!') == -1: return -1
+            n = self.nLine.text()
+            if self.check_for_empty_input(n, 'Prázdný klíč N! Pro dešifrování je '
+                                          'potřeba zadat N a D.') == -1: return -1
+            d = self.dLine.text()
+            if self.check_for_empty_input(d, 'Prázdný klíč D! Pro šifrování je '
+                                          'potřeba zadat N a D.') == -1: return -1
+            my_text = self.decrypt(my_text, n, d)
+            my_text = self.decode_transformation(my_text)
+            self.outputText.setPlainText(my_text)
+        except ValueError:
+            self.error_message('Při dešifrování musí být vstup INT!')
         
-        
+    def error_message(self, message):
+        error_message = QMessageBox()
+        error_message.setText(message)
+        error_message.setWindowTitle('Chyba!')
+        error_message.exec()
+    
     def __init__(self):
         QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
         self.randGenButton.clicked.connect(self.generate_key)
         self.encodeButton.clicked.connect(self.encodeButton_clicked)
+        self.decodeButton.clicked.connect(self.decodeButton_clicked)
      
 if __name__ == "__main__":
     app = QApplication(sys.argv)
