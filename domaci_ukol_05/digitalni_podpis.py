0afbb7d68d7d5d170becbb09d1d25db86cf1c692
Martin <darebnik.martin01@gmail.com>
2021-12-02 18:41:50 +0100

    ukol5

---
+++ domaci_ukol_05/digitalni_podpis.py
@@ -0,0 +1,55 @@
+import sys
+import hashlib
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
+from PyQt5 import QtGui, uic
+
+
+qtCreatorFile = "gui.ui" # Enter file here.
+ 
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+ 
+class MyApp(QMainWindow, Ui_MainWindow):
+    
+    def get_hash(self, file):
+        BLOCK_SIZE = 65536 # The size of each read from the file
+        file_hash = hashlib.sha3_512() # Create the hash object, can use something other than `.sha256()` if you wish
+        with open(file, 'rb') as f: # Open the file to read it's bytes
+            fb = f.read(BLOCK_SIZE) # Read from the file. Take in the amount declared above
+            while len(fb) > 0: # While there is still data being read from the file
+                file_hash.update(fb) # Update the hash
+                fb = f.read(BLOCK_SIZE) # Read the next block from the file
+        return (file_hash.hexdigest()) # Get the hexadecimal digest of the hash
+    
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+        if fileName:
+            return fileName
+        
+    def error_message(self, message):
+        error_message = QMessageBox()
+        error_message.setText(message)
+        error_message.setWindowTitle('Chyba!')
+        error_message.exec()
+        
+    def chooseFileButton_clicked(self, line):
+        file = self.openFileNameDialog()
+        if line == 0:
+            self.filePath.setText(file)
+        elif line == 1:
+            self.zipFilePath.setText(file)
+    
+    def __init__(self):
+        QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.chooseFileButton.clicked.connect(self.chooseFileButton_clicked, 0)
+        self.chooseFileButton_2.clicked.connect(self.chooseFileButton_clicked, 1)
+     
+        
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
